11f1a17ff5c15a346fb9fe0c20e5fb812799bd2c
Agorist-Action <agorist_action@protonmail.com>
2021-08-31 23:36:51 -0500

    Remove TradingServerCS

--- src/TradingEngine/TradingEngineServer.cs
+++ src/TradingEngine/TradingEngineServer.cs
@@ -12,14 +12,10 @@ using System.Threading.Tasks;
 using TradingEngineServer.Core.Configuration;
 using TradingEngineServer.Logging;
 using TradingEngineServer.Exchange;
-using TradingEngineServer.Server;
 using TradingEngineServer.Orders;
 using TradingEngineServer.Orderbook;
 using TradingEngineServer.Rejects;
 
-using TradingEngineServer.Orders;
-using TradingEngineServer.Orderbook;
-using TradingEngineServer.Rejects;
 using TradingEngineServer.OrderEntryCommunication;
 using System.Linq;
 using TradingEngineServer.Orders.OrderStatuses;
@@ -41,10 +37,6 @@ namespace TradingEngineServer.Core
             _textLogger = textLogger ?? throw new ArgumentNullException(nameof(textLogger));
 
         }
-
-<<<<<<< HEAD
-        public Task CancelAllAsync(List<IOrderStatus> orderStatuses)
-=======
         public Task<ExchangeResult> ProcessOrderAsync(Order order)
         {
             _textLogger.Information(nameof(TradingEngineServer), $"Handling NewOrder: {order}");
@@ -101,8 +93,7 @@ namespace TradingEngineServer.Core
             else return Task.FromResult(ExchangeResult.CreateExchangeResult(RejectionCreator.GenerateOrderCoreReject(cancelOrder, RejectionReason.OrderbookNotFound)));
         }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
->>>>>>> master
+        public Task CancelAllAsync(List<IOrderStatus> orderStatuses)
         {
             _textLogger.Information(nameof(TradingEngineServer), $"Unsolicited cancel for all active orders: {string.Join(", ", orderStatuses.Select(x => x.OrderId))}");
             var cancelTasks = orderStatuses.Select(x =>
@@ -112,61 +103,6 @@ namespace TradingEngineServer.Core
             return Task.WhenAll(cancelTasks);
         }
 
-        public Task<ExchangeResult> ProcessOrderAsync(Order order)
-        {
-            _textLogger.Information(nameof(TradingEngineServer), $"Handling NewOrder: {order}");
-            if (_exchange.TryGetOrderbook(order.SecurityId, out var orderbook))
-            {
-                if (RejectGenerator.TryRejectNewOrder(order, orderbook, out var rejection))
-                {
-                    return Task.FromResult(ExchangeResult.CreateExchangeResult(rejection));
-                }
-                else
-                {
-                    orderbook.AddOrder(order);
-                    var matchResults = orderbook.Match();
-                    return Task.FromResult(ExchangeResult.CreateExchangeResult(matchResults.Fills));
-                }
-            }
-            else return Task.FromResult(ExchangeResult.CreateExchangeResult(RejectionCreator.GenerateOrderCoreReject(order, RejectionReason.OrderbookNotFound)));
-        }
-
-        public Task<ExchangeResult> ProcessOrderAsync(ModifyOrder modifyOrder)
-        {
-            _textLogger.Information(nameof(TradingEngineServer), $"Handling ModifyOrder: {modifyOrder}");
-            if (_exchange.TryGetOrderbook(modifyOrder.SecurityId, out var orderbook))
-            {
-                if (RejectGenerator.TryRejectModifyOrder(modifyOrder, orderbook, out var rejection))
-                {
-                    return Task.FromResult(ExchangeResult.CreateExchangeResult(rejection));
-                }
-                else
-                {
-                    orderbook.ChangeOrder(modifyOrder);
-                    var matchResults = orderbook.Match();
-                    return Task.FromResult(ExchangeResult.CreateExchangeResult(matchResults.Fills));
-                }
-            }
-            else return Task.FromResult(ExchangeResult.CreateExchangeResult(RejectionCreator.GenerateOrderCoreReject(modifyOrder, RejectionReason.OrderbookNotFound)));
-        }
-
-        public Task<ExchangeResult> ProcessOrderAsync(CancelOrder cancelOrder)
-        {
-            _textLogger.Information(nameof(TradingEngineServer), $"Handling CancelOrder: {cancelOrder}");
-            if (_exchange.TryGetOrderbook(cancelOrder.SecurityId, out var orderbook))
-            { 
-                if (RejectGenerator.TryRejectCancelOrder(cancelOrder, orderbook, out var rejection))
-                {
-                    return Task.FromResult(ExchangeResult.CreateExchangeResult(rejection));
-                }
-                else
-                {
-                    orderbook.RemoveOrder(cancelOrder);
-                    return Task.FromResult(ExchangeResult.CreateExchangeResult());
-                }
-            }
-            else return Task.FromResult(ExchangeResult.CreateExchangeResult(RejectionCreator.GenerateOrderCoreReject(cancelOrder, RejectionReason.OrderbookNotFound)));
-        }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
