b4c1d8183db32857aa6192ff2ad075346737f160
Agorist-Action <agorist_action@protonmail.com>
2021-10-08 17:34:25 -0500

    Add Singular API Calls to Interface

--- src/MarketDataCS/IMarketDataPublisher.cs
+++ src/MarketDataCS/IMarketDataPublisher.cs
@@ -4,12 +4,16 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using TradingEngineServer.OrderbookData;
+using TradingEngineServer.Orders;
 
 namespace TradingEngineServer.MarketData
 {
     interface IMarketDataPublisher
     {
+        Task PublishTradeAsync(Trades.Trade trades, CancellationToken token);
         Task PublishTradesAsync(List<Trades.Trade> trades, CancellationToken token);
+        Task PublishIncrementalOrderbookUpdateAsync(IncrementalOrderbookUpdate incrementalOrderbookUpdates, CancellationToken token);
         Task PublishIncrementalOrderbookUpdatesAsync(List<IncrementalOrderbookUpdate> incrementalOrderbookUpdates, CancellationToken token);
+        Task PublishIncrementalOrderbookUpdateAsync(OrderbookSlice obs, CancellationToken token);
     }
 }
